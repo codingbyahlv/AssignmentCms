c0a4b3da633750780ee46bbf556af9a38cb1e782
codingbyahlv <code@ahlv.se>
2024-09-10 14:52:07 +0200

    services

---
+++ AssignmentCms/wwwroot/scripts/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll('a.btn[data-should-center]');
+
+    buttons.forEach(function(button) {
+        var shouldCenter = button.getAttribute('data-should-center') === 'True';
+        var parentDiv = button.closest('div[data-content-element-type-alias="buttonLink"]');
+
+        if (shouldCenter && parentDiv) {
+            parentDiv.style.textAlign = 'center';
+        } else if (parentDiv) {
+            parentDiv.style.textAlign = 'left';
+        }
+    });
+});
+
+
+
+
+
+
